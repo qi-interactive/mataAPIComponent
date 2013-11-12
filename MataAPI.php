4d93d53671c70fdc40c6e12951791ba0342c386c
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2013-11-12 14:56:50 +0000

    added authentication envelope
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- MataAPI.php
+++ MataAPI.php
@@ -1,48 +1,49 @@
 <?php
+
 class MataAPI extends CComponent {
 
-	public function init() {
-		if (!function_exists('curl_version')) 
-			throw new CException("Curl is required for " . __CLASS__ . " to function");
-	}
+    public function init() {
+        if (!function_exists('curl_version'))
+            throw new CException("Curl is required for " . __CLASS__ . " to function");
+    }
 
+    public function get($url, array $params = array(), $authentication) {
+        return $this->callURL("GET", $url, $params, $authentication);
+    }
 
-	public function get($url, array $params = array()) {
-		return $this->callURL("GET", $url, $params);
-	}
+    public function post($url, array $params = array()) {
+        return $this->callURL("POST", $url, $params);
+    }
 
-	public function post($url, array $params = array()) {
-		return $this->callURL("POST", $url, $params);
-	}
+    private function callURL($method, $url, $data = false, $authentication) {
+        $curl = curl_init();
+        switch ($method) {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
 
-	private function callURL($method, $url, $data = false) {
-		$curl = curl_init();
-		switch ($method) {
-			case "POST":
-			curl_setopt($curl, CURLOPT_POST, 1);
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
 
-			if ($data)
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-			break;
-			case "PUT":
-			curl_setopt($curl, CURLOPT_PUT, 1);
-			break;
-			default:
-			if ($data)
-				$url = sprintf("%s?%s", $url, http_build_query($data));
-		}
+        if ($authentication != null) {
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($curl, CURLOPT_USERPWD, $authentication);
+        }
 
-		// if ($authenticate) {
-		// 	curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		// 	curl_setopt($curl, CURLOPT_USERPWD, $userCredentials);
-		// }
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        return json_decode(curl_exec($curl));
+    }
 
-		return json_decode(curl_exec($curl));
-	}
 }
 
 ?>
