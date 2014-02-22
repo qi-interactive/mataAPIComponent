90b87b06766e46f382f311767afc937686cd8f6c
Marcin Wiatr <mail.wichura@gmail.com>
2014-02-22 13:11:30 +0000

    improved error handling a bit
    Signed-off-by: Marcin Wiatr <mail.wichura@gmail.com>

--- MataAPI.php
+++ MataAPI.php
@@ -16,21 +16,21 @@ class MataAPI extends CComponent {
     }
 
     private function callURL($method, $url, $data = false, $authentication = null) {
-                      
+      
         $curl = curl_init();
         switch ($method) {
             case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POST, 1);
 
-                if ($data)
-                    curl_setopt($curl, CURLOPT_POSTFIELDS,  http_build_query($data, '', '&'));
-                break;
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS,  http_build_query($data, '', '&'));
+            break;
             case "PUT":
-                curl_setopt($curl, CURLOPT_PUT, 1);
-                break;
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
             default:
-                if ($data)
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
         }
 
         if ($authentication != null) {
@@ -42,7 +42,13 @@ class MataAPI extends CComponent {
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         
-        return json_decode(curl_exec($curl));
+        $response = curl_exec($curl);
+        $responseJSON = json_decode($response);
+        
+        if ($responseJSON == null)
+            throw new CHttpException(500, "Could not perform query to Web Service: " . $response);
+
+        return $responseJSON;
     }
 
 }
